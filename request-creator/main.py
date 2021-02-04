6dc13bf1e54c7eb8f476fecc7ab07c02850940db
Matt Patrick <hazmat345@gmail.com>
2021-02-03 19:44:31 -0500

    Adding waiting and time infomation printing

--- request-creator/main.py
+++ request-creator/main.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+from concurrent.futures import wait
+from datetime import datetime
 from pathlib import Path
 
 from brewtils import SystemClient
@@ -13,8 +15,22 @@ def main():
     req = {"_"+k: v for k, v in raw_req.items() if k != "parameters"}
     req.update(raw_req.get("parameters", {}))
 
-    sys_client = SystemClient(**load_config())
-    sys_client.send_bg_request(**req)
+    futures = []
+    sys_client = SystemClient(**load_config(), blocking=False)
+
+    start = datetime.now()
+
+    for _ in range(250_000):
+        futures.append(sys_client.send_bg_request(**req))
+    create_end = datetime.now()
+
+    wait(futures)
+    wait_end = datetime.now()
+
+    print(f"Started: {start}")
+    print(f"Ended: {wait_end}")
+    print(f"Creation took: {create_end - start}")
+    print(f"Total time: {wait_end - start}")
 
 
 if __name__ == '__main__':
