7646a948895357b15be6df32b8c0411bd1733b28
TheBurchLog <5104941+TheBurchLog@users.noreply.github.com>
2025-01-17 09:40:55 -0500

    Adding rate metrics

--- event_handler_test/request_stress_test.py
+++ event_handler_test/request_stress_test.py
@@ -5,12 +5,12 @@ from concurrent.futures import ThreadPoolExecutor, wait
 import bson
 import requests
 import time
-
+import random
 hostname = "localhost"
 url = f"http://{hostname}:2337/api/v1/forward"
 garden = "default"
 child_garden = "child"
-
+my_list = [0.1, 0.2, 0.3, 0.4, 0.5]
 
 def current_time():
     localized = datetime.datetime.now(datetime.timezone.utc)
@@ -25,6 +25,7 @@ def post_event(event):
 
 def create_request_events(request_id, loop):
 
+    time.sleep( random.choice(my_list))
     create_time = current_time()
     event = {
         "name": "REQUEST_CREATED",
@@ -69,27 +70,35 @@ def create_request_events(request_id, loop):
     event["payload"]["status"] = "IN_PROGRESS"
     event["payload"]["status_updated_at"] = current_time()
 
+    time.sleep( random.choice(my_list))
     post_event(operation)
 
     event["name"] = "REQUEST_COMPLETED"
     event["payload"]["status"] = "SUCCESS"
     event["payload"]["status_updated_at"] = current_time()
 
+    time.sleep( random.choice(my_list))
     post_event(operation)
 
 
 def generate_random_mongo_id():
     return str(bson.ObjectId())
 
+import time
+
+print("Starting Event Generator")
 for loop in range(3000):
-    print("Starting Event Generator")
+    
+    start_time = time.time()
     with ThreadPoolExecutor(max_workers=50) as executor:
         
             futures = [
-                executor.submit(create_request_events, generate_random_mongo_id(), loop)
+                executor.submit(create_request_events, generate_random_mongo_id(), loop,)
                 for _ in range(1000)
             ]
             wait(futures)
-
-    print("Sleeping for backlog to keep up")
-    time.sleep(3*60)
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    rate = round((1000 * 3) / (elapsed_time / 60), 2)
+    requests_rate = round(1000/ (elapsed_time / 60), 2)
+    print(f"Publishing {rate} Events and {requests_rate} Requests per minute")
